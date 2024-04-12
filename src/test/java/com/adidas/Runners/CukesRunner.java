78672e29b81a52dad286d26f4db8f89915e35345
Onnipa <plareorn@gmail.com>
2024-04-12 10:32:25 -0400

    Adidas Project

---
+++ src/test/java/com/adidas/Runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.adidas.Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/adidas/StepDefs",
+        dryRun = false,
+        tags = "@B32G8-244",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
