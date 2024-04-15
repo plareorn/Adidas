193a10bd11ec61c635c37a4c5705d4edc33c0a2e
Onnipa <plareorn@gmail.com>
2024-04-15 11:48:52 -0400

    Page is done

--- src/test/java/com/adidas/Pages/BasePage.java
+++ src/test/java/com/adidas/Pages/BasePage.java
@@ -1,20 +1,28 @@
 package com.adidas.Pages;
 
+import com.adidas.Utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import javax.swing.text.html.CSS;
 
 public abstract class  BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
-    @FindBy (xpath = "(//a[@class='nav-link'])[1]")
-    public WebElement homeButton;
 
-    @FindBy (id ="cartur")
-    public WebElement cartButton;
+        @FindBy (xpath = "(//a[@class='nav-link'])[1]")
+        public WebElement homeButton;
 
-    @FindBy (css = ".btn-success")
-    public WebElement addToCartButton;
+        @FindBy (id ="cartur")
+        public WebElement cartButton;
 
-}
+        @FindBy (css = ".btn-success")
+        public WebElement addToCartButton;
+
+    }
+
+
--- src/test/java/com/adidas/Pages/HomePage.java
+++ src/test/java/com/adidas/Pages/HomePage.java
@@ -1,9 +1,14 @@
 package com.adidas.Pages;
 
+import com.adidas.Utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage {
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
     @FindBy(xpath = "//a[text()='Laptops']")
     public WebElement laptopButton;
 
